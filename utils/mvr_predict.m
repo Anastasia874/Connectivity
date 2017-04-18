29056edaedf8f0dc42d3001a7a20b05ef1f5b2ba
Anastasia874 <anastasia.motrenko@gmail.com>
2017-04-18 10:17:02 +0300

    mvr

---
+++ utils/mvr_predict.m
@@ -0,0 +1,22 @@
+function [preds, res] = mvr_predict(coeffs, x)
+
+dims = size(x);
+
+if length(dims) == 2
+    [nchan, tmp] = size(coeffs);
+    nlags = tmp / nchan;
+    coeffs = reshape(coeffs, nchan, nchan, nlags);
+else
+    nlags = size(coeffs, 3);
+end
+
+nsamples = size(x, 1);
+preds = zeros(nsamples - nlags, nchan)';
+
+for lag = 1:nlags
+    preds = preds + coeffs(:, :, lag) * x(nlags-lag+1:end-lag, :)';
+end
+preds = preds';
+res = x(nlags+1:end, :) - preds;
+
+end
